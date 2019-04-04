fb53c048361cb666f5cf0501dcede65535027db0
bordini007 <ricardo.sampaio@edu.unipar.br>
2019-04-04 12:11:07 -0300

    Criando Telas no menu Movimentos

--- src/tela/TelaSistemaPrincipal.java
+++ src/tela/TelaSistemaPrincipal.java
@@ -1,4 +1,5 @@
 package tela;
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JDesktopPane;
@@ -7,77 +8,76 @@ import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 
-public class TelaSistemaPrincipal extends JFrame implements ActionListener{
-    //permite jinternalframe aparecer. controle das telas 
+public class TelaSistemaPrincipal extends JFrame implements ActionListener {
+
+    //permite jinternalframe aparecer. controle das telas
     public static JDesktopPane jdp = new JDesktopPane();
-    
-    
+
     //------ criando menus --------- //
-    
     public JMenuBar jmb = new JMenuBar();
     public JMenu jmCadastros = new JMenu("Cadastros");
     public JMenu jmMovimentos = new JMenu("Movimentos");
     public JMenu jmRelatorios = new JMenu("Relatorios");
-    
+
     // ---- colocando os itens de menus ---------//
-    
     public JMenuItem jmiEstado = new JMenuItem("Estado");
     public JMenuItem jmiCidade = new JMenuItem("Cidade");
     public JMenuItem jmiFornecedor = new JMenuItem("Fornecedor");
-    
-    
 
+    // ----- criando os itens de menus movimentos e relatorios -------//
+    public JMenuItem jmiProdutoCadas = new JMenuItem("Cadastro de Produtos");
+    public JMenuItem jmiCompras = new JMenuItem("Entrada de Produtos");
+    public JMenuItem jmiVendas = new JMenuItem("Saida de Produtos");
 
-    public TelaSistemaPrincipal() {       
-        //----------tela do sistema ----------// 
+    public TelaSistemaPrincipal() {
+        //----------tela do sistema ----------//
         getContentPane().add(jdp);
         //jdp.setBackground(Color.LIGHT_GRAY);
-        
+
         setTitle("Sistema de Cadastro");
         //setSize(800,600);
         setExtendedState(MAXIMIZED_BOTH);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-        
+
         setJMenuBar(jmb);
         jmb.add(jmCadastros);
         jmb.add(jmMovimentos);
         jmb.add(jmRelatorios);
-        
-        //adicionar dentro do menu cadastro 
+
+        //adicionar dentro do menu cadastro
         jmCadastros.add(jmiEstado);
         jmCadastros.add(jmiCidade);
         jmCadastros.add(jmiFornecedor);
-        
-        // direcionar cada menu 
+
+        //adicionar dentro do menu movimentos
+        jmMovimentos.add(jmiProdutoCadas);
+        jmiProdutoCadas.add(jmiCompras);
+        jmiProdutoCadas.add(jmiVendas);
+
+        // direcionar cada menu
         jmiEstado.addActionListener(this);
         jmiCidade.addActionListener(this);
         jmiFornecedor.addActionListener(this);
-        
-        
+
         setVisible(true);
     }
 
     @Override
     public void actionPerformed(ActionEvent ae) {
         // getSource == quem foi que gerou o listerne
-        if (ae.getSource() == jmiEstado ) {
-           TelaDeCadastroEstado telaDeCadastroEstado = new TelaDeCadastroEstado();
-           jdp.add(telaDeCadastroEstado);
-        }
-        else if(ae.getSource() == jmiCidade){
+        if (ae.getSource() == jmiEstado) {
+            TelaDeCadastroEstado telaDeCadastroEstado = new TelaDeCadastroEstado();
+            jdp.add(telaDeCadastroEstado);
+        } else if (ae.getSource() == jmiCidade) {
             TelaDeCadastroCidade telaDeCadastroCidade = new TelaDeCadastroCidade();
             jdp.add(telaDeCadastroCidade);
-            
+
+        } else if (ae.getSource() == jmiFornecedor) {
+            TelaDeCadastroFornecedor telaDeCadastroFornecedor = new TelaDeCadastroFornecedor();
+            jdp.add(telaDeCadastroFornecedor);
+
         }
-        else if(ae.getSource() == jmiFornecedor){
-             TelaDeCadastroFornecedor telaDeCadastroFornecedor = new TelaDeCadastroFornecedor();
-             jdp.add(telaDeCadastroFornecedor);
-             
-        }
-        
-    
+
     }
-    
-    
-    
+
 }
